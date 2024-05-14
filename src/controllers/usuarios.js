b17e67e202201f95e86e81a424a8cc8b1cbae526
Manuel Garcia <m.gd@outlook.es>
2024-05-14 11:30:30 +0200

    Creacion del registro

--- src/controllers/usuarios.js
+++ src/controllers/usuarios.js
@@ -1,31 +1,48 @@
-const bcrypt = require('bcrypt');
-const db = require('../database/models');
+const bcrypt = require("bcrypt");
+const fs = require("fs");
+const path = require("path");
+const db = require("../database/models");
 
-const UsuarioModel = db.getModel('Usuarios')
+const UsuarioModel = db.getModel("Usuarios");
 
 const crearUsuario = async (req, res) => {
-    try {
-        
-        const contrasenaCifrada = await bcrypt.hash(nuevoUsuario.Contraseña, 10);
-
-        const nuevoUsuario = await UsuarioModel.create({
-            Nombre: req.body.Nombre,
-            Email: req.body.Email,
-            Contraseña: contrasenaCifrada,
-            Foto: req.body.Foto, 
-            Nick: req.body.Nick,
-            FechaCreacion: new Date(),
-            RolID: req.body.RolID,
-            Estado: 'inactivo'
-        });
-        console.log(nuevoUsuario)
-
-        res.status(201).json(nuevoUsuario);
-    } catch (error) {
-        res.status(500).json(error)
+  try {
+    const existeUsuarioEmail = await UsuarioModel.findOne({
+      Email: req.body.Email,
+    });
+    if (existeUsuarioEmail) {
+      return res.status(400).json({ error: "El usuario ya existe" });
     }
-}
+    const contrasenaCifrada = await bcrypt.hash(req.body.Contraseña, 10);
+
+    const imgPath = req.body.Foto;
+
+    if (!fs.existsSync(imgPath)) {
+      return res.status(400).json({ error: "Imagen no encontrada" });
+    }
+
+    const imgBase64 = `data:image/jpeg;base64,${fs
+      .readFileSync(imgPath)
+      .toString("base64")}`;
+
+    const nuevoUsuario = await UsuarioModel.create({
+      Nombre: req.body.Nombre,
+      Email: req.body.Email,
+      Contraseña: contrasenaCifrada,
+      Foto: imgBase64,
+      Nick: req.body.Nick,
+      FechaCreacion: new Date(),
+      RolID: req.body.RolID,
+      Estado: "inactivo",
+    });
+
+    res.status(201).json(nuevoUsuario);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    console.error(error);
+  }
+};
 
 module.exports = {
-    crearUsuario
+  crearUsuario,
 };
