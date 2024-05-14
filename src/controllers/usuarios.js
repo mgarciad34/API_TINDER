204ef1dce55aadce247907c2b5b95bb6e12d4291
Manuel Garcia <m.gd@outlook.es>
2024-05-14 11:58:13 +0200

    Actualizamos creacion registro de usuario para no duplicar emails

--- src/controllers/usuarios.js
+++ src/controllers/usuarios.js
@@ -7,24 +7,34 @@ const UsuarioModel = db.getModel("Usuarios");
 
 const crearUsuario = async (req, res) => {
   try {
+    // Buscar si ya existe un usuario con el correo electrónico proporcionado
     const existeUsuarioEmail = await UsuarioModel.findOne({
-      Email: req.body.Email,
+      where: { Email: req.body.Email },
     });
+
+    // Si existe un usuario con ese correo electrónico, devolver un error
     if (existeUsuarioEmail) {
-      return res.status(400).json({ error: "El usuario ya existe" });
+      return res
+        .status(400)
+        .json({ error: "El correo electrónico ya está en uso" });
     }
+
+    // Si no existe un usuario con ese correo electrónico, proceder a crear el nuevo usuario
     const contrasenaCifrada = await bcrypt.hash(req.body.Contraseña, 10);
 
     const imgPath = req.body.Foto;
 
+    // Verificar si la imagen existe
     if (!fs.existsSync(imgPath)) {
       return res.status(400).json({ error: "Imagen no encontrada" });
     }
 
+    // Leer la imagen y convertirla a base64
     const imgBase64 = `data:image/jpeg;base64,${fs
       .readFileSync(imgPath)
       .toString("base64")}`;
 
+    // Crear el nuevo usuario en la base de datos
     const nuevoUsuario = await UsuarioModel.create({
       Nombre: req.body.Nombre,
       Email: req.body.Email,
@@ -36,8 +46,10 @@ const crearUsuario = async (req, res) => {
       Estado: "inactivo",
     });
 
+    // Enviar la respuesta con el nuevo usuario creado
     res.status(201).json(nuevoUsuario);
   } catch (error) {
+    // Manejar cualquier error que ocurra durante el proceso
     res.status(500).json({ error: error.message });
     console.error(error);
   }
